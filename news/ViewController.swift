5a9cceeaa6839aeea1873634375a6ca4de56b36a
Roman Dvorytskyi <roman.dvorytskyi@gmail.com>
2020-02-05 15:46:23 +0200

    Initial Commit

---
+++ news/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  news
+//
+//  Created by Roman Dvoritskiy on 05.02.2020.
+//  Copyright © 2020 Roman Dvoritskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
